d24d3d36eb734564e58cafb32fdb40ca806141d9
LightningMan204 <Iamsmartyknown@outlook.com>
2023-10-08 19:55:05 +0530

    Some area for you Take it or Leave it

--- C++/circumference/circumference.cpp
+++ C++/circumference/circumference.cpp
@@ -1,21 +1,6 @@
 #include <iostream>
 using namespace std;
 
-void area(){
-    string yn;
-    cout << "Do you want to calculate the area of the circle? y or n\n";
-    cin >> yn;
-    switch(yn){
-        case "y":
-       
-    }
-}
-
-
-
-
-
-
 int main(){
     double rad;
     double pi = 3.141592653589793;
@@ -30,5 +15,18 @@ int main(){
     cout << "jk here's your answer for the circumference" << endl;
     cout << circumference << endl;
     cout << "btw circumference / perimeter of circle = 2pir or also 2 * pi * r" << endl;
-    return 0;
+
+    string yn;
+    cout << "Do you want to calculate the area too? y or n" << endl;
+    cin >> yn;
+    if (yn == "y")
+    {
+        cout << "ok\n";
+        double area;
+        area = pi * (rad * rad);
+        cout << "Area = " << area << '\n';
+    }else {
+        cout << "Ok\n";
     }
+    return 0;
+}
